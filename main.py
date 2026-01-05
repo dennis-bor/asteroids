ac93825bedd3610323bfdb9d3e30ca7c9ea41d14
dennis-bor <githubprivateaccount.opposite934@passmail.net>
2026-01-05 14:35:40 +0100

    added player movement and updated code to use groups

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ from player import Player
 
 def main():
     pygame.init()
-    dt = 0
-    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
         screen.fill('black')
-        dt = clock.tick(60) / 1000
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
